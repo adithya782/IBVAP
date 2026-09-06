8b7e437813a8246006eb124e1687cd9686cb5fa5
adithya782 <srisaiadithya782@gmail.com>
2026-09-06 15:04:41 +0530

    made minor changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,30 +1,26 @@
 from fastapi import FastAPI
 
+from app.database import engine, Base
+from app.Model import models
+
 from app.api.camera import router as camera_router
 from app.api.event import router as event_router
 
 
+Base.metadata.create_all(bind=engine)
+
+
 app = FastAPI(
-    title="SecureStack IBVAP"
+    title="SecureStack IBVAP API"
 )
 
 
-# ==========================================
-# API ROUTES
-# ==========================================
-
 app.include_router(camera_router)
-
 app.include_router(event_router)
 
 
-# ==========================================
-# ROOT
-# ==========================================
-
 @app.get("/")
 def root():
-
     return {
         "message": "SecureStack IBVAP API is running!"
     }
